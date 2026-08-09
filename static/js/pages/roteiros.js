18bcdc3bce817a8f27073a513fa10b7d1a933347
Tiago <tiago2904santos@gmail.com>
2026-08-09 18:31:11 -0300

    fix(front): preserva import do entrypoint movido (NOVO-73)

--- static/js/pages/roteiros.js
+++ static/js/pages/roteiros.js
@@ -1,4 +1,4 @@
-import { initRoteirosEditor } from './pages/roteiros/editor/index.js';
+import { initRoteirosEditor } from './roteiros/editor/index.js';
 
 function bootRoteirosEditor() {
   const form = document.getElementById('roteiro-editor-form');
